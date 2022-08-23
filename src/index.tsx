cd6160b45cb5da8afbe8c0cee70f3499cc2df6bb
tamashiro-syuta <syuta5825@gmail.com>
2022-08-23 23:19:24 +0900

    reactのコードが反映されるまでの順序

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 【Reactのコードが反映されるまでの順序】
+// 1.public/index.htmlが読み込まれて、ブラウザで表示
+// 2.ブラウザがjsのコードを取得し、Reactで書いたコードを実行
+// 3.render()で与えられたAppを、root作成時に与えられたrootというidを持つ要素以下に描画する
+
+// idがrootの要素を指定
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
